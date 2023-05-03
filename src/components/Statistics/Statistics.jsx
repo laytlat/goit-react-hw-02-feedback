bd8725b5f3f1cb6fe854a70dcd4b6caecd2d9054
Alexandr Hrynenko <layt.lat@ukr.net>
2023-05-03 17:40:43 +0300

    deleted useless import

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Notification } from 'components/Notification/Notification';
 
 import {
   StatisticsContainer,
